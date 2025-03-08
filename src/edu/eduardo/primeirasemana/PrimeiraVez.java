e44293eba5481c2d852cef0b73adbd6403eb5e37
unknown <edubriggs@gmail.com>
2025-03-08 15:05:04 -0300

    first commit

---
+++ src/edu/eduardo/primeirasemana/PrimeiraVez.java
@@ -0,0 +1,12 @@
+package edu.eduardo.primeirasemana;
+
+public class PrimeiraVez {
+	public static void main(String[] args) {
+		System.out.println("Olá mundo");
+		String [] emails = {"opi", "teste","salve", "empregobom"};
+		for (int i=0; i<emails.length; i++) {
+			System.out.println(emails[i]);
+		}
+		
+	}
+}
